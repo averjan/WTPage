d9b596797aa24f1d2a61289507b642bc8257cd23
Averjan <timophey.krut@gmail.com>
2021-03-16 02:08:02 +0300

    Add templates for all files

---
+++ private/views/ingredients/ingredients.php
@@ -0,0 +1,28 @@
+<?php
+define("root", $_SERVER['DOCUMENT_ROOT']);
+
+$main_template = file_get_contents(root . "/index.html");
+
+$main_template = str_replace("{header}",
+    file_get_contents(root . "/common/header.html"),
+    $main_template);
+
+$main_template = str_replace("{footer}",
+    file_get_contents(root . "/common/footer.html"),
+    $main_template);
+
+$main_template = str_replace("{navbar}",
+    file_get_contents(root . "/common/navbar.html"),
+    $main_template);
+
+$main_template = str_replace("{description}",
+    file_get_contents("ingredients.html"),
+    $main_template);
+
+$main_template = str_replace("{sub-navbar}",
+    file_get_contents(root . "/common/sub-navbar.html"),
+    $main_template);
+
+$temp = 12;
+echo $main_template;
+// include "ingredients.html";
---
+++ private/views/signup/signup.php
@@ -0,0 +1,22 @@
+<?php
+define("root", $_SERVER['DOCUMENT_ROOT']);
+
+$main_template = file_get_contents(root . "/index.html");
+
+$main_template = str_replace("{header}",
+    file_get_contents(root . "/common/header.html"),
+    $main_template);
+
+$main_template = str_replace("{footer}",
+    file_get_contents(root . "/common/footer.html"),
+    $main_template);
+
+$main_template = str_replace("{navbar}",
+    file_get_contents(root . "/common/navbar.html"),
+    $main_template);
+
+$main_template = str_replace("{description}",
+    file_get_contents("signup.html"),
+    $main_template);
+
+echo $main_template;
