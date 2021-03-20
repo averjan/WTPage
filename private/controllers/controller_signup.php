48a673e7d75f9f76a3f183334d017d138315f6d0
Averjan <timophey.krut@gmail.com>
2021-03-20 21:43:08 +0300

    Add email validation for signup page
    Validates email in controller_signup. if matches, writes username and email to \src\mail.txt
    TODO: if not matches, reload signup page with a hint

--- private/controllers/controller_signup.php
+++ private/controllers/controller_signup.php
@@ -6,4 +6,21 @@ class controller_signup extends Controller
     {
         $this->view->generate("/signup/signup.php");
     }
+
+    function action_registration()
+    {
+        $mail = $_POST["email"];
+        $reg = "/^[0-9a-zA-Z]+(\.[0-9a-zA-Z]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?$/";
+        if (preg_match($reg, $mail))
+        {
+            $work_file = fopen(root."\src\mail.txt", "a+");
+            fwrite($work_file, $_POST["username"] . " " . $mail . "\n");
+            fclose($work_file);
+            $this->view->generate("/home/home.php");
+        }
+        else
+        {
+            $this->view->generate("/signup/signup.php");
+        }
+    }
 }
