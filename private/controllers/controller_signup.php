25dc33fe4416d22f71930754cd0b6c3b90688a34
Averjan <timophey.krut@gmail.com>
2021-05-04 21:54:53 +0300

    Sending mail

--- private/controllers/controller_signup.php
+++ private/controllers/controller_signup.php
@@ -1,5 +1,14 @@
 <?php
 
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
 class controller_signup extends Controller
 {
     public string $mail_error = "";
@@ -17,6 +26,7 @@ class controller_signup extends Controller
         $reg = "/^(?:\s*)([\d\w!#$%&'*+\/=?^_`{|}~-]+(?:\.[\d\w!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z\d](?:[a-z\d-]*[a-z\d])?\.)+[a-z\d](?:[a-z\d-]*[a-z\d])?)(?:\s*)$/";
         if (preg_match($reg, $mail, $match))
         {
+            $this->sendMail($mail, $_POST["username"]);
             $work_file = fopen(root."\src\mail.txt", "a+");
             fwrite($work_file, $_POST["username"] . " " . $match[1] . "\n");
             fclose($work_file);
@@ -26,6 +36,39 @@ class controller_signup extends Controller
         {
             $mail_error = "Incorrect email";
             $this->view->generate("/signup/signup.php", $mail_error);
+        }
+    }
+
+    private function sendMail(string $mail_string, string $user)
+    {
+        $mail = new PHPMailer(true);
+        try {
+            //Server settings
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'courseworkksis@gmail.com';                     //SMTP username
+            $mail->Password   = '15testPassword';                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = 587;
+
+            //Recipients
+            $mail->setFrom('elgusto@elgusto.com', 'El Gusto');
+            //$mail->setFrom('course_work_ksis@mail.ru', 'El Gusto');
+            $mail->addAddress($mail_string, $user);     //Add a recipient
+            //$mail->addAddress('ellen@example.com');               //Name is optional
+            $mail->addReplyTo('courseworkksis@gmail.com', 'El Gusto');
+
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Welcome!';
+            $mail->Body    = 'Welcome to ElGusto, <bold>'.$user.'</bold>!';
+            $mail->AltBody = 'Welcome to ElGusto, '.$user.'!';
+
+            $mail->send();
+            //echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
     }
 }
